e58710c4f4a4e95249d4d45940f797fb6f4a7ff4
Anne <anne.grose68@gmail.com>
2015-07-20 20:31:46 -0500

    1st and 2nd cheer

---
+++ csharp/Cheers/Cheers/CheerUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Cheers
+{
+	public class CheerUser
+	{
+		public CheerUser ()
+		{
+			
+		}
+	}
+}
+
+//### What to Implement
+//1. Getting user's name and print it.
+//2. Cheer the user on.
+//
+//For example, if name is "Bob", output should be:
+//
+//	```shell
+//	Give me a.. b
+//	Give me an.. o
+//	Give me a.. b
+//	BOB is.. GRAND!
+//	```
---
+++ csharp/Cheers/Cheers/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Cheers
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Please input your name");
+			string text = Console.ReadLine ();
+			string greeting = ("days until your birthday:");
+
+
+			Console.WriteLine ("Please input your Birthday (month:00 and day:00)");
+			string date = Console.ReadLine ();
+			DateTime dateOBJ = DateTime.Parse (date);
+
+
+			var daysTillBday = (DateTime.Today.Subtract(dateOBJ).Days);
+
+			if (daysTillBday == 0) 
+			{
+				greeting = ("Happy Birthday!!!!");
+			}
+
+			if (daysTillBday > 0)
+			{
+				daysTillBday = 365 - daysTillBday;
+			}
+			if (daysTillBday < 0) 
+			{
+				daysTillBday = daysTillBday * -1;
+			}
+
+			//Array[] letters = text.Split("");
+			//System.Console.WriteLine("{0} words in text:", words.Length);
+
+			foreach (char s in text)
+			{
+				System.Console.WriteLine("Give me a...  " + s);
+			}
+			Console.WriteLine (greeting + " " + daysTillBday);
+
+			// Keep the console window open in debug mode.
+
+			// Keep the console window open in debug mode.
+
+
+
+
+		}
+	}
+
+}
